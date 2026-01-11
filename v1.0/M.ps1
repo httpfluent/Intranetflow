72df080b9b4ff53e761d276939ac08d535e3f5dd
httpfluent <abirboss293636@gmail.com>
2026-01-11 19:46:09 +0600

    Update M.ps1

--- v1.0/M.ps1
+++ v1.0/M.ps1
@@ -3,6 +3,7 @@
 # TOTAL SILENCE - NO WINDOW - NO ERRORS
 # ===================================================
 
+
 $InstallDir = "$env:LOCALAPPDATA\Programs\Python\Python312"
 $PythonExe = Join-Path $InstallDir "python.exe"
 $InstallRequired = $true
